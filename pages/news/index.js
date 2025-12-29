01d2cb7586a0eab0331c06d9d749a5a48d38cf07
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2025-12-29 19:52:56 +1000

    Using Link component to link between pages:

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,23 @@
 // my-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react/jsx-runtime";
+
 function NewsPage() {
-    return <h1>The news page</h1>
+    return (
+        <Fragment>
+            {/* Fragment: groups sibling elements without adding an extra DOM node */}
+            <h1>The news page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-framework">News Item 1</Link>
+                </li>
+                <li>
+                    <Link href="/news/learn-nextjs">News Item 2</Link>
+                </li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default NewsPage;
