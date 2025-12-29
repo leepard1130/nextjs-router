b0264a9c0eda1f8372b8067a83c930b4ebf33691
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2025-12-29 19:28:38 +1000

    project setup

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// my-domain.com
+
+function HomePage() {
+    return <h1>The home page</h1>
+}
+
+export default HomePage;
---
+++ pages/news/index.js
@@ -0,0 +1,7 @@
+// my-domain.com/news
+
+function NewsPage() {
+    return <h1>The news page</h1>
+}
+
+export default NewsPage;
